b276da84231153ccf8772611abb6e9a268e04e9f
Jvzqz13 <j.vzqz13@gmail.com>
2024-01-30 23:01:48 -0500

    finished app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
+
 function App() {
+  //API KEY
+  const apiKey = '2cb5861b';
+  
+  //State to hold movie data
+  const [ movie, setMovie ] = useState(null);
+
+  // Make fetch request and stores the response
+  const getMovie = async(searchTerm) => {
+    try { 
+       const response = await fetch (
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    const data = await response.json();
+    setMovie(data);
+
+  } catch (e) {
+    console.error(e);
+  }
+
+  };
+
+  useEffect (() => { getMovie("Cluecless") },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <h1> Movie React </h1>
+      
+      {/* passed the getMovie function as a prop called moviesearch */}
+      <Form moviesearch={getMovie} />
+
+     {/* passed movie as props to movie display */}
+      <MovieDisplay movie={movie} />
+
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+
+function Form(props){
+
+    // State to hold the data of our form 
+    const [ formData, setFormData ] = useState({searchterm:""});
+
+        // Updates formData when we type into form
+    const handleChange = (e) => {
+        // Use event object to detect key, and value to update
+        setFormData({...formData, [e.target.name]: e.target.value })
+    };
+
+    const handleSubmit = (e) => {
+        // Prevent page from refreshing on form submission
+        e.preventDefault();
+        // Pass the search term to moviesearch prop, which is Apps function
+        props.moviesearch(formData.searchterm);
+    }
+
+
+    return (
+        <div>
+
+            {/* <h1> The Form Component </h1> */}
+            <form onSubmit={handleSubmit}> 
+                <input type="text" name="searchterm" value={formData.searchterm} onChange={handleChange} />
+                <input type="submit" value="submit" />
+            </form>
+        </div>
+        )
+
+};
+
+export default Form;
